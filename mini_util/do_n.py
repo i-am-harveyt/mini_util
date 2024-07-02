6acbaee05b80b4992d140964f3436cbbc6effccf
Harvey Tung <harvey.tung@onedegree.hk>
2024-07-02 15:23:13 +0800

    v0.1.4

--- mini_util/do_n.py
+++ mini_util/do_n.py
@@ -16,3 +16,21 @@ class DoN:
             return wrapper
 
         return inner
+
+
+def do_n(n: int):
+    """
+    Do function n times
+
+    Args
+        n (int): the number you want to repeat
+    """
+    def inner(f):
+        def wrapper(*args, **kwargs):
+            ret = [None] * n
+            for i in range(n):
+                ret[i] = f(*args, **kwargs)
+
+        return wrapper
+
+    return inner
