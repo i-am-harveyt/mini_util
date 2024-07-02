100d5b39a75f1ff4bcd8c36623cb17715ae2bb13
Harvey Tung <harvey.tung@onedegree.hk>
2024-07-02 18:01:22 +0800

    Fix bad command

--- mini_util/cli.py
+++ mini_util/cli.py
@@ -3,7 +3,7 @@
 from argparse import ArgumentParser
 
 
-def main(s: str = "hello cli") -> None:
+def main() -> None:
     """
     This is the main function, print `s` on screen
 
@@ -12,13 +12,13 @@ def main(s: str = "hello cli") -> None:
     Returns:
         None
     """
-    print(s)
-
-
-if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
         "-s", "--string", help="The string to print", type=str, required=True
     )
     args = parser.parse_args()
-    main(args.string)
+    print(args.string)
+
+
+if __name__ == "__main__":
+    main()
