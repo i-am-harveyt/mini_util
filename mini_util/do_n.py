10e8135c469a61daab01ec1b7c682bd2aeb73ae2
Harvey Tung <harvey.tung@onedegree.hk>
2024-07-02 15:57:35 +0800

    change structure

--- mini_util/do_n.py
+++ mini_util/do_n.py
@@ -1,3 +1,22 @@
+def do_n(n: int):
+    """
+    Do function n times
+
+    Args
+        n (int): the number you want to repeat
+    """
+    def inner(f):
+        def wrapper(*args, **kwargs):
+            ret = [None] * n
+            for i in range(n):
+                ret[i] = f(*args, **kwargs)
+
+        return wrapper
+
+    return inner
+
+
+
 class DoN:
     @staticmethod
     def do_n(n: int):
@@ -16,5 +35,3 @@ class DoN:
             return wrapper
 
         return inner
-
-
